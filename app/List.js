c5520df8e43f37bc1386df4b296f8330b095aa0e
Gunnari Auvinen <gauvinen@gmail.com>
2015-03-27 13:36:05 -0700

    Update List component with App functionality.

---
+++ app/List.js
@@ -0,0 +1,38 @@
+var React = require('react');
+var Todos = require('./Todos');
+var TodoForm = require('./TodoForm');
+
+var List = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: ['First Todo', 'Second Todo', 'Third Todo']
+    };
+  },
+
+  addTodo: function (text) {
+    this.setState({
+      todos: this.state.todos.concat([text])
+    });
+  },
+
+  removeTodo: function (index) {
+    var newTodos = this.state.todos.slice(0);
+    newTodos.splice(index, 1);
+    this.setState({
+      todos: newTodos
+    });
+  },
+
+  render: function() {
+    return (
+      <div>
+        <TodoForm addTodo={this.addTodo} />
+        <Todos
+          todos={this.state.todos}
+          removeTodo={this.removeTodo} />
+      </div>
+    );
+  }
+});
+
+module.exports = List;
