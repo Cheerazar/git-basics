c417716dd160cf8903d6fcd668d4b9dbfa1810f6
Gunnari Auvinen <gauvinen@gmail.com>
2015-03-27 13:34:43 -0700

    Refactor majority of App functionality into List.

--- app/App.js
+++ app/App.js
@@ -1,35 +1,11 @@
 var React = require('react');
-var Todos = require('./Todos');
-var TodoForm = require('./TodoForm');
+var List = require('./List');
 
 var App = React.createClass({
-  getInitialState: function() {
-    return {
-      todos: ['First Todo', 'Second Todo', 'Third Todo']
-    };
-  },
-
-  addTodo: function (text) {
-    this.setState({
-      todos: this.state.todos.concat([text])
-    });
-  },
-
-  removeTodo: function (index) {
-    var newTodos = this.state.todos.slice(0);
-    newTodos.splice(index, 1);
-    this.setState({
-      todos: newTodos
-    });
-  },
-
   render: function() {
     return (
       <div>
-        <TodoForm addTodo={this.addTodo} />
-        <Todos
-          todos={this.state.todos}
-          removeTodo={this.removeTodo} />
+        <List />
       </div>
     );
   }
